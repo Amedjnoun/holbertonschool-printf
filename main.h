2bfd3708a9a851a61726e0a02cb56ddf50b633cc
stan-holberton <9857@holbertonstudents.com>
2024-11-29 18:07:38 +0000

    modification du fichier print_task_0

--- main.h
+++ main.h
@@ -8,8 +8,5 @@ int _printf(const char *format, ...);
 int print_char(va_list args);
 int print_string(va_list args);
 int handle_format(const char *format, va_list args);
-int print_integer(va_list args);
-int print_integer_recursive(int n);
-int print_unsigned_recursive(unsigned int n);
 
 #endif /* MAIN_H */
