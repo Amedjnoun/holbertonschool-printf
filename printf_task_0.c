dbeaf7c2cb195331a924b6c567f6d478f04f9734
Amedjnoun <A.medjnoun@gmail.com>
2024-11-29 09:55:25 +0100

    printf

--- printf_task_0.c
+++ printf_task_0.c
@@ -1,74 +1,121 @@
 #include "main.h"
+#include <stdarg.h>
+#include <unistd.h>
 
 /**
- * _printf - Produit une sortie selon un format donné
- * @format: La chaîne de format contenant des spécificateurs
+ * print_char - Gère l'affichage d'un caractère.
+ * @args: Liste des arguments.
  *
- * Return: Le nombre de caractères imprimés (hors octet nul)
+ * Return: Nombre de caractères imprimés.
+ */
+int print_char(va_list args)
+{
+	/* Récupération du caractère depuis la liste des arguments */
+	char c = va_arg(args, int);
+
+	/* Utilisation de write pour écrire le caractère sur stdout */
+	return (write(1, &c, 1));
+}
+
+/**
+ * print_string - Gère l'affichage d'une chaîne.
+ * @args: Liste des arguments.
+ *
+ * Return: Nombre de caractères imprimés.
+ */
+int print_string(va_list args)
+{
+	/* Récupération de la chaîne de caract depuis la liste des arguments */
+	char *str = va_arg(args, char *);
+	int count = 0;
+
+	/* Si la chaîne est NULL, afficher "(null)" à la place */
+	if (!str)
+		str = "(null)";
+
+	/* Parcours de la chaîne et écriture caractère par caractère */
+	while (*str)
+		count += write(1, str++, 1);
+
+	return (count);
+}
+/**
+ * print_integer - Gère l'affichage d'un entier.
+ * @args: Liste des arguments.
+ * Return: Nombre de caractères imprimés.
+ */
+int print_integer(va_list args)
+{
+	/* Récupération de l'entier depuis la liste des arguments */
+	int n = va_arg(args, int);
+	return (print_integer_recursive(n));
+}
+
+/**
+ * handle_format - Gère les spécificateurs de format.
+ * @format: La chaîne de format.
+ * @args: Liste des arguments.
+ *
+ * Return: Nombre de caractères imprimés.
+ */
+int handle_format(const char *format, va_list args)
+{
+	int count = 0;
+
+	/* Vérification du spécificateur de format */
+	if (*format == 'c')
+		/* Affichage d'un caractère */
+		count += print_char(args);
+	else if (*format == 's')
+		/* Affichage d'une chaîne de caractères */
+		count += print_string(args);
+	else if (*format == '%')
+		/* Affichage du caractère '%' */
+		count += write(1, "%", 1);
+	else if (*format == 'd' || *format == 'i')
+		/* Affichage d'un entier */
+		count += print_integer(args);
+	else
+		/* Affichage du caractère inconnu tel quel */
+		count += write(1, format, 1);
+
+	return (count);
+}
+
+/**
+ * _printf - Produit une sortie selon un format donné.
+ * @format: La chaîne de format contenant des spécificateurs.
+ *
+ * Return: Nombre de caractères imprimés (hors octet nul).
  */
 int _printf(const char *format, ...)
 {
-	va_list args;
+	va_list args; /* Liste des arguments variables */
 	int i = 0, count = 0;
 
-	/* Vérification si le format est NULL */
+	/* Si le format est NULL, retourner une erreur */
 	if (!format)
 		return (-1);
 
-	/* Initialisation de la liste d'arguments */
+	/* Initialisation de la liste des arguments */
 	va_start(args, format);
 
-	/* Boucle sur chaque caractère de la chaîne de format */
+	/* Parcours de la chaîne de format */
 	while (format && format[i])
 	{
-		/* Vérification d'un spécificateur de format (%) */
+		/* Si un spécificateur de format est détecté */
 		if (format[i] == '%')
-		{
-			i++;
-			/* Cas du spécificateur de caractère ('c') */
-			if (format[i] == 'c')
-			{
-				char c = va_arg(args, int);
-				write(1, &c, 1); /* Affichage du caractère */
-				count++;
-			}
-			/* Cas du spécificateur de chaîne ('s') */
-			else if (format[i] == 's')
-			{
-				char *str = va_arg(args, char *);
-
-				/* Si la chaîne est NULL, afficher "(null)" */
-				if (!str)
-					str = "(null)";
-				while (*str)
-				{
-					write(1, str++, 1); /* Affichage des caractères */
-					count++;
-				}
-			}
-			/* Cas du spécificateur de pourcentage ('%') */
-			else if (format[i] == '%')
-			{
-				write(1, "%", 1); /* Affichage du caractère '%' */
-				count++;
-			}
-			/* Si le spécificateur est inconnu, on l'affiche tel quel */
-			else
-			{
-				write(1, &format[i], 1);
-				count++;
-			}
-		}
-		/* Si ce n'est pas un spécificateur, on affiche le caractère */
+			/* Gérer le format et passer au caractère suivant */
+			count += handle_format(&format[++i], args);
 		else
-		{
-			write(1, &format[i], 1);
-			count++;
-		}
+			/* Écrire directement le caractère */
+			count += write(1, &format[i], 1);
 		i++;
 	}
 
-	/* Fin de la liste d'arguments */
+	/* Nettoyage de la liste des arguments */
 	va_end(args);
-	return (count); /* Retourne le nombre de caractères imprimés */
+
+	/* Retourner le nombre total de caractères imprimés */
+	return (count);
 }
