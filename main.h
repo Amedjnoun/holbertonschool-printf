9ae9098fcf4c2125291d18bca8c33d153059a751
stan-holberton <9857@holbertonstudents.com>
2024-11-29 09:04:45 +0000

    ajout des déclarations de fonction dans main.h pour la fonction _ printf

--- main.h
+++ main.h
@@ -7,5 +7,9 @@
 /* Prototype de la fonction _printf */
 int _printf(const char *format, ...);
 int print_integer_recursive(int n);
+int print_char(va_list args); 
+int print_string(va_list args); 
+int handle_format(const char *format, va_list args);
+
 
 #endif /* MAIN_H */
